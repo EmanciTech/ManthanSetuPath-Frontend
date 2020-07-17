6ea903583c7c1f247baa9955983c793b71bd4422
harsh19punjani <punjaniharsh@gmail.com>
2020-07-17 09:32:38 +0530

    client side changes

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -6,10 +6,13 @@ import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { ContentComponent } from './component/content/content.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ConstructionComponent } from './construction/construction.component';
+import { EnvironmentalComponent } from './environmental/environmental.component';
+import { MedicalComponent } from './medical/medical.component';
 
 
 @NgModule({
-  declarations: [ClientComponent, HeaderComponent, FooterComponent, ContentComponent],
+  declarations: [ClientComponent, HeaderComponent, FooterComponent, ContentComponent, ConstructionComponent, EnvironmentalComponent, MedicalComponent],
   imports: [
     CommonModule,
     TranslateModule
