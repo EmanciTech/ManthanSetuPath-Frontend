6148a061bd15b34621dc1442bbeea8d7b60131a3
Arjun Khetia <arjunkhetia@gmail.com>
2020-07-20 22:08:38 +0530

    3 modules services added

--- src/app/admin/component/construction/construction.component.ts
+++ src/app/admin/component/construction/construction.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConstructionComponent implements OnInit {
 
+  public imageSrc: any = 'assets/images/imageplaceholder.png';
+  public categories = [
+    {id: 1, category: 'Construction Category'}
+  ];
+  public services = [
+    {id: 1, image: 'assets/images/imageplaceholder.png', category: 'Construction Category',
+    name: 'Construction Service', description: 'Construction Service Description'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectImage(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        this.imageSrc = event.target.result;
+      }
+    }
   }
 
 }
--- src/app/admin/component/medical/medical.component.ts
+++ src/app/admin/component/medical/medical.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicalComponent implements OnInit {
 
+  public imageSrc: any = 'assets/images/imageplaceholder.png';
+  public categories = [
+    {id: 1, category: 'Medical Category'}
+  ];
+  public services = [
+    {id: 1, image: 'assets/images/imageplaceholder.png', category: 'Medical Category',
+    name: 'Medical Service', description: 'Medical Service Description'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectImage(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        this.imageSrc = event.target.result;
+      }
+    }
   }
 
 }
