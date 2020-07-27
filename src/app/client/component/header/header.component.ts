1e39f8dabcc22cce202836373a4ba46a726b8434
harsh19punjani <punjaniharsh@gmail.com>
2020-07-27 15:09:29 +0530

    latest push 27-07-2020

--- src/app/client/component/header/header.component.ts
+++ src/app/client/component/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../../service/data.service';
 declare var $: any;
 
 
@@ -10,9 +11,20 @@ declare var $: any;
 })
 export class HeaderComponent implements OnInit {
   public menu = '';
-  constructor(private router: Router) { }
+  public construction = [];
+  public medical = [];
+  public environmental = [];
+  constructor(private router: Router, private service: DataService) { }
 
   ngOnInit(): void {
+    if (!sessionStorage.getItem('construction') && !sessionStorage.getItem('construction') && !sessionStorage.getItem('construction')) {
+      this.getMenu();
+    } else {
+      this.construction = JSON.parse(sessionStorage.getItem('construction'));
+      this.medical = JSON.parse(sessionStorage.getItem('medical'));
+      this.environmental = JSON.parse(sessionStorage.getItem('environmental'));
+    }
+
     if (this.router.url.includes('construction')) {
       this.menu = 'construction';
     } else if (this.router.url.includes('environmental')) {
@@ -22,17 +34,34 @@ export class HeaderComponent implements OnInit {
     } else if (this.router.url.includes('aboutus')) {
       this.menu = 'aboutus';
     }
-    else if (this.router.url.includes('aboutus')) {
+    else if (this.router.url.includes('contactus')) {
       this.menu = 'contactus';
     }
- }
   }
 
-  // make navbar top andd fix based on window scroll 
-  $(window).scroll(function(){
-    var sticky = $('.navbar'),
-        scroll = $(window).scrollTop();
-  
-    if (scroll >= 60) sticky.addClass('sticky');
-    else sticky.removeClass('sticky');
-  });
+  getMenu() {
+    this.service.getMenu()
+      .subscribe(
+        data => {
+          this.construction = data['data']['construction'];
+          this.medical = data['data']['medical'];
+          this.environmental = data['data']['environmental'];
+          sessionStorage.setItem('construction', JSON.stringify(this.construction));
+          sessionStorage.setItem('medical', JSON.stringify(this.medical));
+          sessionStorage.setItem('environmental', JSON.stringify(this.environmental));
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+}
+
+// make navbar top andd fix based on window scroll 
+$(window).scroll(function () {
+  var sticky = $('.navbar'),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= 60) sticky.addClass('sticky');
+  else sticky.removeClass('sticky');
+});
--- src/app/client/service/data.service.ts
+++ src/app/client/service/data.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000';
+  constructor(private http: HttpClient, private router: Router) { }
+
+  
+  public getMenu(): Observable < any > {
+    return this.http
+      .get(this.baseUrl + '/dashboard')
+      .pipe(catchError(this.handleError));
+  }
+   // throw error to component
+   public handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Client-side Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Server-side Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
 }
